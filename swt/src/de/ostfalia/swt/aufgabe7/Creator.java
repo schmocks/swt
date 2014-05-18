aea1600b4d28cb281eb8dfa3e0ed365baceba19b
Master <sdegener@xxx.de>
2014-05-18 11:40:12 +0200

    12

---
+++ swt/src/de/ostfalia/swt/aufgabe7/Creator.java
@@ -0,0 +1,31 @@
+package de.ostfalia.swt.aufgabe7;
+
+import java.io.File;
+
+public class Creator {
+	
+	/**
+	 * Erzeugt im Verzeichnis workingDir eine Klasse className
+	 * mit der Klassendefinition classDefinition
+	 * 
+	 * @param workingDir Das Verzeichnis
+	 * @param className Der voll qualifizierte Klassenname
+	 * @param classDefinition Die Definition der Klasse
+	 * @return Die erzeugt Datei
+	 * @throws Exception
+	 */
+	
+	public File createClassFile(File workingDir, String className, String classDefinition) throws Exception{
+		return null;
+	}
+	/**
+	 * Compiliert die in classFile übergebene Klasse
+	 * @param classFile
+	 * @return true falls Compilation erfolgreich
+	 */
+	
+	public boolean compileClassFile(File classFile){
+		return false;
+	}
+	
+}
---
+++ swt/src/de/ostfalia/swt/aufgabe7/testm.java
@@ -0,0 +1,10 @@
+package de.ostfalia.swt.aufgabe7;
+
+public class testm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
